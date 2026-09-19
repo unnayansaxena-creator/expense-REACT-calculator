c7e8ed273be9d4003242f608261331ebec9c5b76
unnayansaxena-creator <unnayansaxena@gmail.com>
2026-09-19 22:57:41 +0530

    Update App.jsx
    Fix CSS import case

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
-import "./styles/app.css";
+import "./styles/App.css";
 
 import Home from "./pages/Home";
 import Review from "./pages/Review";
